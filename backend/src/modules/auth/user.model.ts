9bbbdd45ae1b1cbc8c498774fc3e4da7a5a4f2eb
Abhinav Naman <abhinav.n@sourceasia.co.in>
2026-02-14 20:59:23 +0530

    add User schema

---
+++ backend/src/modules/auth/user.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+}, { timestamps: { createdAt: true, updatedAt: false } })
+
+export default mongoose.model<IUser>('User', UserSchema);
